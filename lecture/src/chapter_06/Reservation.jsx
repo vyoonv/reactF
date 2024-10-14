afc86f7a537fe475001d56a903a997b5e002e3ae
vyoonv <156267745+vyoonv@users.noreply.github.com>
2024-10-14 16:25:13 +0900

    reservation

---
+++ lecture/src/chapter_06/Reservation.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+function Reservation(props) {
+
+    const [haveBreakfast, setHaveBreakfast] = useState(true); 
+    const [numberOfGuest, setNumberOfGuest] = useState(2); 
+
+    const handleSubmit = (event) => {
+        alert(`아침식사 여부 : ${haveBreakfast}, 방문객수 : ${numberOfGuest}`); 
+        event.preventDefault(); 
+    }
+
+  return (
+    <form>
+        <label>
+            아침식사 여부 : 
+            <input type='checkbox'
+                   checked={haveBreakfast}
+                   onChange={(event) => {
+                    setHaveBreakfast(event.target.checked); 
+                   }} />
+        </label>
+        <br />
+        <label>
+            방문객 수 : 
+            <input type='number'
+                   value={numberOfGuest}
+                   onChange={(event) => {
+                    setNumberOfGuest(event.target.value); 
+                   }} 
+            />
+        </label>
+        <button type='submit'>제출</button>
+    </form>
+  )
+}
+
+export default Reservation
--- lecture/src/index.js
+++ lecture/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
+import fruit from './chapter_07/Fruit';
 
 // React18부터 ReactDOM.render()는 사용되지 않고 createRoot를 사용함 
 // ReactDOM.createRoot()를 사용
@@ -13,6 +14,7 @@ import CommentList from './chapter_05/CommentList';
 root.render(
   <React.StrictMode>
     <CommentList />
+    <Fruit />
   </React.StrictMode>
 );
 
